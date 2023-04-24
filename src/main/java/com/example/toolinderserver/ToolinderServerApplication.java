4c39c037e8772a1dee4090eacd73e54e6d8d98bc
Y-Joo <wldudwn99@gmail.com>
2023-04-24 15:25:26 +0900

    Initial Commit

---
+++ src/main/java/com/example/toolinderserver/ToolinderServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.toolinderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolinderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolinderServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toolinderserver/ToolinderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toolinderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolinderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
